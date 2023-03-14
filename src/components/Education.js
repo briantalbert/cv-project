6981e48b02bf365f7b7d4fa8c8e9ce70922f0fe1
Brian Talbert <BrianLTalbert@gmail.com>
2023-03-14 11:24:35 -0400

    Initial commit

---
+++ src/components/Education.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+}
